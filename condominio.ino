a289250ee5d272f5859dff6eb5ef8afdf1d5faa2
Day <dayanerl.dev@gmail.com>
2022-11-29 20:55:24 -0300

    Add files via upload

--- condominio.ino
+++ condominio.ino
@@ -1,25 +1,48 @@
+// C++ code
+#include <Ethernet.h> //habilidade necessária para que o nosso Arduino, ao estar atuando como um servidor web, seja capaz de conversar com um cliente que esteja conectado a ele
+#include <MySQL_Connection.h> //promover a conexão Arduino UNO/servidor MySQL e executar as sentenças de manipulação de informações em um banco de dados
+#include <MySQL_Cursor.h> //promover a conexão Arduino UNO/servidor MySQL e executar as sentenças de manipulação de informações em um banco de dados
+#include <SPI.h> //é um protocolo de dados seriais síncronos utilizado em microcontroladores para comunicação entre o microcontrolador e um ou mais periféricos
 
-#include <Ethernet.h>
-#include <MySQL_Connection.h>
-#include <MySQL_Cursor.h>
-#include <SPI.h>
 
-#define LM35 A2
+//DECLARAÇÃO DE INPUTS
+#define SenAgua A0 //Fotorresistor representando um Sensor de Agua, cabo Turquesa
+#define SenLuz A1 //Fotorresistor representando um Sensor de Luz, cabo Roxo
+#define ButAgua 2 //Botão Agua, Cabo Rosa
+#define ButLuz 3 //Botão Luz, Cabo Rosa
+#define SenPres 4 //Sensor de Presença, cabo Marrom
+//DECLARAÇÃO DE OUTPUTS
+#define LedAgua 12 //Led informativo do fluxo de agua corrente, cabo Azul
+#define LedLuz 11 //Led informativo de consumo de energia, cabo Amarelo
+#define ReleAgua 8 //Rele representando uma válvula solenóide, cabo Roxo
+#define ReleLuz 9 //Rele, cabo Rosa
+#define Lampada 5 //Led representando uma lampada, cabo Verde
+//Os leds conectados aos reles não tem ligação em portas arduíno
 
+//configurando conexao
+byte mac_addr[] = { 0x00, 0x27, 0x13, 0xAE, 0x79, 0x0F }; //informar MAC host
+IPAddress server_addr(10,10,117,14); //informar ip host
+
+//acesso ao BD
+char user[] = "arduino";         
+char password[] = "arduino123";        
+
+//STRING PARA BANCO
+char INSERIR_TEMP[] = "INSERT INTO registrotemp (temperatura) VALUES (%s)";
+char BANCODEDADOS[] = "USE internet_shield";
+EthernetClient client;
+MySQL_Connection conn((Client *)&client);
+
+//leitura temperatura
 int leitura;
 float leituraconvertida;
 char sentenca[128];
 char valortemp[10];
-byte mac_addr[] = { 0x00, 0x27, 0x13, 0xAE, 0x79, 0x0F };
 
-IPAddress server_addr(10,10,117,14);
-char user[] = "arduino";              
-char password[] = "arduino123";        
-char INSERIR_TEMP[] = "INSERT INTO registrotemp (temperatura) VALUES (%s)";
-char BANCODEDADOS[] = "USE internet_shield";
 
-EthernetClient client;
-MySQL_Connection conn((Client *)&client);
+
+
+
 
 void setup() 
 { 
@@ -44,6 +67,8 @@ void setup()
    
 }
 
+
+//TROCAR NOME LM35 POR SENSOR CORRETO
 void loop() 
 {
    Serial.println("Executando sentença");
